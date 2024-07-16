d8e6cd49b82153c8672b14ccc947c06f486b1dcf
RamezCh <ramiz_chreide@outlook.com>
2024-07-17 00:09:34 +0300

    Fix Bug

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function BillInput({ bill, onSetBill }) {
         type="text"
         placeholder="Bill value"
         value={bill}
-        onChange={e => onSetBill(e.target.value)}
+        onChange={e => onSetBill(Number(e.target.value))}
       />
     </div>
   );
@@ -54,11 +54,14 @@ function SelectPercentage({ percentage, onSelect, children }) {
   return (
     <div>
       <label>{children} </label>
-      <select value={percentage} onChange={e => onSelect(e.target.value)}>
-        <option value="0">Dissastisfied (0%)</option>
-        <option value="5">It was okay (5%)</option>
-        <option value="10">It was good (10%)</option>
-        <option value="20">Absolutely amazing! (20%)</option>
+      <select
+        value={percentage}
+        onChange={e => onSelect(Number(e.target.value))}
+      >
+        <option value={0}>Dissastisfied (0%)</option>
+        <option value={5}>It was okay (5%)</option>
+        <option value={10}>It was good (10%)</option>
+        <option value={20}>Absolutely amazing! (20%)</option>
       </select>
     </div>
   );
