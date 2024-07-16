959d49e04b9ed8de0ff91a6be6d2407f21ceba0d
RamezCh <ramiz_chreide@outlook.com>
2024-07-16 23:46:36 +0300

    BillInput

--- src/App.js
+++ src/App.js
@@ -17,7 +17,14 @@ function TipCalculator() {
   );
 }
 
-function BillInput() {}
+function BillInput() {
+  return (
+    <div>
+      <label>How much was the bill?</label>
+      <input type="text" placeholder="Bill value" />
+    </div>
+  );
+}
 
 function SelectPercentage() {}
 
